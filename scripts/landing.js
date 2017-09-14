1a50d35af5ba0273177850b2793726c15b3a0c91
Monica Berkowitz <monicaberkowitz@gmail.com>
2017-09-13 21:14:25 -0400

    changed nav and fixed page margins

--- scripts/landing.js
+++ scripts/landing.js
@@ -10,27 +10,7 @@ function nextSlide(){
 
 
 $(document).ready(function() {
-    $("#navToggle a").click(function(e){
-        e.preventDefault();
-
-        $("header > nav").slideToggle("medium");
-        $("#logo").toggleClass("menuUp menuDown");
-    });
-
-    $(window).resize(function() {
-        if($( window ).width() >= "600") {
-            $("header > nav").css("display", "block");
-
-            if($("#logo").attr('class') == "menuDown") {
-                $("#logo").toggleClass("menuUp menuDown");
-            }
-        }
-        else {
-            $("header > nav").css("display", "none");
-        }
-    });
-
-    $("header > nav.mainMenu > ul > li > a").click(function(e) {
+	$("header > nav.mainMenu > ul > li > a").click(function(e) {
         if($( window ).width() <= "600") {
             if($(this).siblings().size() > 0 ) {
                 $(this).siblings().slideToggle("fast")
