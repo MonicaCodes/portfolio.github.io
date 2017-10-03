ae33eeb9c848141d7b160e1d0e27f9b9e19d8d73
Monica Berkowitz <monicaberkowitz@gmail.com>
2017-10-03 18:56:28 -0400

    added contact form

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,23 +1,16 @@
-/*
-var slides = document.querySelectorAll('#slides .slide');
-var currentSlide = 0;
-var slideInterval = setInterval(nextSlide,8000);
-
-function nextSlide(){
-	slides[currentSlide].className = 'slide';
-	currentSlide = (currentSlide+1)%slides.length;
-	slides[currentSlide].className = 'slide showing';
-}
-
-
-$(document).ready(function() {
-	$("header > nav.mainMenu > ul > li > a").click(function(e) {
-        if($( window ).width() <= "600") {
-            if($(this).siblings().size() > 0 ) {
-                $(this).siblings().slideToggle("fast")
-				$(this).children(".toggle").html($(this).children(".toggle").html() == 'close' ? 'expand' : 'close');
-            }
-        }
-    });
+/*jQuery(document).ready(function($) {
+	//create the slider
+	$('.mb-testimonials-wrapper').flexslider({
+		selector: ".mb-testimonials > li",
+		animation: "slide",
+		controlNav: true,
+		slideshow: false,
+		smoothHeight: true,
+		start: function() {
+			$('.mb-testimonials').children('li').css({
+				'opacity': 1,
+				'position': 'relative'
+			});
+		}
+	});
 });
-/*
