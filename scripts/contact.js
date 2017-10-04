941f59365461cdaf1d11cba614432904ac383056
Monica Berkowitz <monicaberkowitz@gmail.com>
2017-10-04 16:47:17 -0400

    added case study pages

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,7 +1,8 @@
 <div id="wufoo-z1bdjc5s00n5fun">
 Fill out my <a href="https://monicaberkowitz.wufoo.com/forms/z1bdjc5s00n5fun">online form</a>.
 </div>
-<script type="text/javascript">var z1bdjc5s00n5fun;(function(d, t) {
+<script type="text/javascript">
+var z1bdjc5s00n5fun;(function(d, t) {
 var s = d.createElement(t), options = {
 'userName':'monicaberkowitz',
 'formHash':'z1bdjc5s00n5fun',
@@ -10,10 +11,19 @@ var s = d.createElement(t), options = {
 'async':true,
 'host':'wufoo.com',
 'header':'show',
-'ssl':true};
+'ssl':true
+};
 s.src = ('https:' == d.location.protocol ? 'https://' : 'http://') + 'www.wufoo.com/scripts/embed/form.js';
 s.onload = s.onreadystatechange = function() {
-var rs = this.readyState; if (rs) if (rs != 'complete') if (rs != 'loaded') return;
-try { z1bdjc5s00n5fun = new WufooForm();z1bdjc5s00n5fun.initialize(options);z1bdjc5s00n5fun.display(); } catch (e) {}};
-var scr = d.getElementsByTagName(t)[0], par = scr.parentNode; par.insertBefore(s, scr);
-})(document, 'script');</script>
+var rs = this.readyState;
+if (rs) if (rs != 'complete') if (rs != 'loaded')
+return;
+try {
+    z1bdjc5s00n5fun = new WufooForm();z1bdjc5s00n5fun.initialize(options);
+    z1bdjc5s00n5fun.display();
+}
+catch (e) {}};
+var scr = d.getElementsByTagName(t)[0], par = scr.parentNode; 
+par.insertBefore(s, scr);
+})(document, 'script');
+</script>
